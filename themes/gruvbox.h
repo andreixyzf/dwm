f43c6ace38a55c3ac188ec253eca5c0e575d69c2
Andrew <121280646+andreixyzf@users.noreply.github.com>
2025-07-31 16:58:07 +0300

    dwm

---
+++ themes/gruvbox.h
@@ -0,0 +1,21 @@
+
+/* GRUVBOX COLORS */
+#define BLACK              "#282828"
+#define RED                "#cc241d"
+#define GREEN              "#98971a"
+#define YELLOW             "#d79921"
+#define BLUE               "#458588"
+#define PURPLE             "#b16286"
+#define AQUA               "#689d6a"
+#define ORANGE             "#d65d0e"
+#define WHITE              "#ebdbb2"
+#define GRAY               "#32302f"
+
+#define BG_COLOR           BLACK
+#define BORDER_COLOR       GRAY
+#define FG_COLOR           WHITE
+#define SEL_FG_COLOR       YELLOW
+#define SEL_BORDER_COLOR   ORANGE
+#define SEL_BG_COLOR       BLACK
+
+#define FONT               "Iosevka:size=11"
